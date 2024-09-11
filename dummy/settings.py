a3f6e92d1dffe62dab39c2206c20a06405897115
Arjun C <arjun@daveanit.com>
2024-09-11 18:08:38 +0530

    First Commit 3

--- dummy/settings.py
+++ dummy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-01aa28vw=^&j-@25c@#*v8a7+14yh-ka9nu7g+n4q8!2%h(y3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','192.168.1.174']
 
 
 # Application definition
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
